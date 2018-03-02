626013c74542cef345a2080da7c87f04586e2b8d
kimkundad <kim.kundad@gmail.com>
2018-03-02 15:56:31 +0700

    gool

--- mail.php
+++ mail.php
@@ -1,40 +1,51 @@
 <?php
-session_start();
+/**
+ * This example shows making an SMTP connection with authentication.
+ */
 
-//require('db_connect.php');
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Asia/Bangkok');
 
-if (!isset($_POST["mto"])){
-  $course = "hi\nthis is test\nthis is line.";
-  $mto = "zashischool@yahoo.com";
-  $school = "Za-shi School";
-  $name = "promthepc@yahoo.com";
-  $email = "promthepc@yahoo.com";
-  $tel = "0899214549";
+require 'PHPMailer/PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+//Set the hostname of the mail server
+$mail->Host = "smtp.gmail.com";
+//Set the SMTP port number - likely to be 25, 465 or 587
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication
+$mail->Username = "learnsbuy@gmail.com";
+//Password to use for SMTP authentication
+$mail->Password = "Ayumusiam168";
+//Set who the message is to be sent from
+$mail->setFrom('kim.kundad@gmail.com', 'Tony jone');
+//Set who the message is to be sent to
+$mail->addAddress('kim.kundad@gmail.com', 'itoffside');
+//Set the subject line
+$mail->Subject = 'itOffside.com test email';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//$mail->msgHTML(file_get_contents('content.html'), dirname(__FILE__));
+$mail->msgHTML("Test email by itoffside.com");
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
-	$name = $_POST["name"];
-	$tel = $_POST["tel"];
-	$email = $_POST["email"];
-	$school = $_POST["school"];
-	$course = $_POST["course"];
-
-	$mto = $_POST["mto"];
+    echo "Message sent!";
 }
-
-// #@#@#@# DATABASE #@#@#@#
-	//$mto = "zashischool@yahoo.com";
-  $mto = "kim.kundad@gmail.com";
-	//$sql = "INSERT INTO reserve(name,tel,email,school,course,date) VALUES ('$name','$tel','$email','$school','$course', NOW())";
-	//mysql_query($sql) or die(mysql_error());
-
-$mes = $name."\n".$school."\n".$email."\n".$tel."\n".$course;
-//$txtfrom = "From: ".$name."\nReply-To: ".$email."\nX-Mailer: PHP/" . phpversion();
-$txtfrom = "From: ".$course."\nReply-To: ".$email."\nX-Mailer: PHP/" . phpversion();
-
-mail($mto, $tel, $mes, $txtfrom );
-
-
-
-$_SESSION['reserve'] = "ok";
-
-header("Location:http://www.za-shi.com/register.php?ok=res");
-?>
